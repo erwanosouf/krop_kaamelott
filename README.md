7bf1090defa986d66914c5fc537a47f1030b051d
Erwan Osouf <erwan.osouf@gmail.com>
2020-05-03 14:56:12 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # krop_kaamelott
 Crops the awful intro theme in Kammelott's episode
+
+Uses ffmpeg-python
+
+ pip install ffmpeg-python
--- krop_kamelott.py
+++ krop_kamelott.py
@@ -10,13 +10,15 @@ def mute(input, output):
     ffmpeg.input(input).output(output, **{ "af": "volume=enable='lt(t,3)':volume=0", "c:v" : "copy"}).overwrite_output().run()
 
 
-os.mkdir("muted")
 
 sources = [ (item, os.path.splitext(item)[1]) for item in os.listdir(".") if os.path.isfile(item) and "kaamelott" in item.lower() ]
 
+output_dir = "muted"
+if not os.path.exists(output_dir) and sources:
+    os.mkdir(output_dir)
 
 for source in sources:
-    mute(source[0], os.path.join("muted", source[0]))
+    mute(source[0], os.path.join(output_dir, source[0]))
 
 
 
